6ca187041d427cabb50ba6b20054cd4f48256ee5
Ronaldfecg <ronaldfecg@gmail.com>
2026-05-04 21:57:18 -0300

    add regra de negócio

--- exConditional.py
+++ exConditional.py
@@ -1,9 +1,15 @@
 valor = float(input("digite o valor do pedido: "))
 
-
+"""
+Regra de negócio:
+* Se a venda for até 100 reais, não tem desconto.
+* Se a venda for entre 100,01 e 299,99, dê 10% de desconto.
+* Se a venda for acima de 300 reais, dê 15% de desconto.
+""" 
 
 if valor <= 100:
-    desconto = 0.95
+   print(f"O valor da compra de R${valor}.")
+   exit()
 
 
 elif valor > 100 and valor < 299.99:
@@ -19,10 +25,9 @@ total = valor * desconto
 descontoPercentual = (1 - desconto) * 100
 descontoPercentual = round(descontoPercentual,0)
 
-# print("valor total foi de: ", total, ". E o desconto foi de: ", descontoPercentual")
+# print("valor total foi de: ", total, ". E o desconto foi de: ", descontoPercentual")"
 
-
-#print(f"Sua compra deu R$ {valor}. Você ganhou {descontoPercentual}%"  f"de desconto. O total agora é R${total}.")
+# print(f"Sua compra deu R$ {valor}. Você ganhou {descontoPercentual}%"  f"de desconto. O total agora é R${total}.")
 
 textoResultado = f"""
 Sua compra deu R$ {valor}.
